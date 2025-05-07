9d7f50b8e0fbdecb080ebbb9aa4c6ca14ffc61bb
andj712 <andj712@gmail.com>
2025-05-07 20:14:08 +0200

    multiple return values

---
+++ multiple-return-value/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1]) //prvo slovo svakog stringa v[:1]
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+}
+
+func main() {
+	fn1, sn1 := getInitials("andjela kovacevic") //fn sn first name, hvata u ta dva stringa
+
+	fmt.Println(fn1, sn1)
+}
